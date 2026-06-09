58ae61d75cb8477eef173c02e62cdf8268e1b0cb
Vader <davepalomado2005@gmail.com>
2026-06-09 13:23:08 +0800

    address update

--- js/config.js
+++ js/config.js
@@ -9,7 +9,7 @@ window.VAPE_SHOP_CONFIG = {
     email: "davepalomado2005@gmail.com",
     facebookUrl: "https://facebook.com/your-vape-shop",
     messengerUrl: "https://m.me/your-vape-shop",
-    address: "Your physical store address",
+    address: "Sta. Barbara, Iloilo City, Philippines",
     mapEmbedUrl: "https://www.google.com/maps/@10.8209467,122.5260011,1739m/data=!3m1!1e3!5m1!1e1?entry=ttu&g_ep=EgoyMDI2MDYwMy4xIKXMDSoASAFQAw%3D%3D=embed",
     hours: [
       "Monday - Saturday: 10:00 AM - 9:00 PM",
